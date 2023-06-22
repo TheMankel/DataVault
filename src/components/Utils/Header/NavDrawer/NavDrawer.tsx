a691f56a66745cd51e2d3f3e77f0045f424b6663
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-06-22 11:15:27 +0200

    Create NavDrawer.tsx

---
+++ src/components/Utils/Header/NavDrawer/NavDrawer.tsx
@@ -0,0 +1,53 @@
+import { Drawer, Box, IconButton, Divider } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+import Logo from '../Logo/Logo';
+import Nav from '../Nav/Nav';
+
+interface IDrawerMenuProps<T> {
+  openDrawer: boolean;
+  title: string;
+  handleOpenDrawer: React.Dispatch<React.SetStateAction<boolean>>;
+  pagesUrls: T[];
+}
+
+const DrawerMenu = <T extends { name: string; url: string }>({
+  openDrawer,
+  handleOpenDrawer,
+  title,
+  pagesUrls,
+}: IDrawerMenuProps<T>) => {
+  return (
+    <Drawer
+      anchor='left'
+      open={openDrawer}
+      onClose={() => handleOpenDrawer(false)}>
+      <Box height={1} textAlign='left'>
+        <Box
+          padding={1}
+          bgcolor='background.default'
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            gap: 4,
+          }}>
+          <Logo title={title} sx={{ display: 'flex' }} />
+          <IconButton
+            color='inherit'
+            aria-label='close drawer'
+            onClick={() => handleOpenDrawer(false)}>
+            <CloseIcon />
+          </IconButton>
+        </Box>
+        <Divider />
+        <Nav
+          pagesUrls={pagesUrls}
+          buttonType={true}
+          closeDrawer={() => handleOpenDrawer(false)}
+        />
+      </Box>
+    </Drawer>
+  );
+};
+
+export default DrawerMenu;
