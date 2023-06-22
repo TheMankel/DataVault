d58a9f70ae94a841a87e9e037cd409ae91f412c1
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-06-22 11:15:19 +0200

    Create NavMenu.tsx

---
+++ src/components/Utils/Header/NavMenu/NavMenu.tsx
@@ -0,0 +1,21 @@
+import { Box, IconButton } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+
+interface INavMenuProps {
+  handleOpenDrawer: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const NavMenu = ({ handleOpenDrawer }: INavMenuProps) => {
+  return (
+    <Box sx={{ display: { xs: 'flex', md: 'none' }, alignItems: 'center' }}>
+      <IconButton
+        color='inherit'
+        aria-label='open drawer'
+        onClick={() => handleOpenDrawer(true)}>
+        <MenuIcon />
+      </IconButton>
+    </Box>
+  );
+};
+
+export default NavMenu;
