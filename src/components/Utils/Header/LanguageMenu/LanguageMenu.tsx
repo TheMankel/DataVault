1f0eed72647a479120acf71fb8049a554dab3222
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-06-23 14:36:07 +0200

    Update LanguageMenu.tsx

--- src/components/Utils/Header/LanguageMenu/LanguageMenu.tsx
+++ src/components/Utils/Header/LanguageMenu/LanguageMenu.tsx
@@ -1,70 +1,50 @@
-import { useState } from 'react';
-import { Box, Button, Paper, List, ListItemButton } from '@mui/material';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
-import { PL, GB } from 'country-flag-icons/react/3x2';
+import { Box, Paper, List } from '@mui/material';
+import LanguageButton from './LanguageButton/LanguageButton';
+import LanguageMenuItem from './LanguageMenuItem/LanguageMenuItem';
+import useLanguageMenu from './hooks/useLanguageMenu';
+
+const languagesFlags = [{ id: 'PL' }, { id: 'GB' }];
 
 const LanguageMenu = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [selectedLanguage, setSelectedLanguage] = useState<string>('PL'); // Add state for the selected language
-
-  const handleOpenMenu = () => {
-    setIsMenuOpen(true);
-  };
-
-  const handleCloseMenu = () => {
-    setIsMenuOpen(false);
-  };
-
-  const handleSelectLanguage = (event: React.MouseEvent<HTMLDivElement>) => {
-    const language = event.currentTarget.id;
-    setSelectedLanguage(language);
-    handleCloseMenu();
-  };
+  const {
+    isMenuOpen,
+    selectedLanguage,
+    handleOpenMenu,
+    handleCloseMenu,
+    handleSelectLanguage,
+  } = useLanguageMenu();
 
   return (
-    <Box
-      onMouseEnter={handleOpenMenu}
-      onMouseLeave={handleCloseMenu}
-      position='relative'>
-      <Button
-        variant='outlined'
-        endIcon={isMenuOpen ? <ArrowDropUpIcon /> : <ArrowDropDownIcon />}
-        sx={{ width: '80px' }}>
-        {selectedLanguage}
-      </Button>
-      {isMenuOpen && (
-        <Paper
-          sx={{
-            position: 'absolute',
-            top: '100%',
-            width: '100%',
-            zIndex: 1300,
-          }}>
-          <List>
-            <ListItemButton
-              id='PL'
-              onClick={handleSelectLanguage}
-              sx={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
-              <PL width={24} style={{ border: '1px solid black' }} />
-            </ListItemButton>
-            <ListItemButton
-              id='GB'
-              onClick={handleSelectLanguage}
-              sx={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
-              <GB width={24} style={{ border: '1px solid black' }} />
-            </ListItemButton>
-          </List>
-        </Paper>
-      )}
+    <Box flex={1} display='flex' justifyContent='flex-end'>
+      <Box
+        position='relative'
+        onMouseEnter={handleOpenMenu}
+        onMouseLeave={handleCloseMenu}>
+        <LanguageButton
+          isMenuOpen={isMenuOpen}
+          selectedLanguage={selectedLanguage}
+          onClick={handleOpenMenu}
+        />
+        {isMenuOpen && (
+          <Paper
+            sx={{
+              position: 'absolute',
+              top: '100%',
+              width: '100%',
+              zIndex: 1300,
+            }}>
+            <List>
+              {languagesFlags.map((flag) => (
+                <LanguageMenuItem
+                  key={flag.id}
+                  id={flag.id}
+                  onClick={handleSelectLanguage}
+                />
+              ))}
+            </List>
+          </Paper>
+        )}
+      </Box>
     </Box>
   );
 };
