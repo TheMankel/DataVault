303ef5f7acde32e1ed629cb6b2ffb2c611d2b9c2
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-07-07 08:53:13 +0200

    Update DataForm.tsx

--- src/components/Home/DataForm/DataForm.tsx
+++ src/components/Home/DataForm/DataForm.tsx
@@ -1,10 +1,16 @@
 import { FormProvider } from 'react-hook-form';
-import { Box, Button, Grid, TextField } from '@mui/material';
+import { Box, Button, Grid } from '@mui/material';
 import useDataForm from './hooks/useDataForm';
 import InputController from '../../Utils/Input/InputController';
+import { useAppSelector } from '../../../store/hooks';
+import { RootState } from '../../../store';
 
 const Form = () => {
   const { methods, handleFormCancel, handleFormSubmit } = useDataForm();
+  const { dataForm, actionButtons } = useAppSelector((state: RootState) => ({
+    dataForm: state.language.data.dataForm,
+    actionButtons: state.language.data.actionButtons,
+  }));
 
   return (
     <FormProvider {...methods}>
@@ -14,70 +20,35 @@ const Form = () => {
         noValidate>
         <Grid container spacing={2}>
           <Grid item xs={6}>
-            <InputController name='firstname' label='Firstname' />
-            {/* <TextField
-              required
-              fullWidth
-              id='firstname'
-              label='Firstname'
-              aria-label='Firstname'
-              type='text'
-            /> */}
+            <InputController name='firstname' label={dataForm.firstname} />
           </Grid>
           <Grid item xs={6}>
-            <InputController name='surname' label='Surname' />
-            {/* <TextField
-              required
-              fullWidth
-              id='surname'
-              label='Surname'
-              aria-label='Surname'
-              type='text'
-            /> */}
+            <InputController name='surname' label={dataForm.surname} />
           </Grid>
           <Grid item xs={12}>
             <InputController
               name='date_of_birth'
-              label='Date of Birth'
+              label={dataForm.date_of_birth}
               type='date'
               InputLabelProps={{ shrink: true }}
             />
-            {/* <TextField
-              required
-              fullWidth
-              id='date_of_birth'
-              label='Date of Birth'
-              aria-label='Date of Birth'
-              type='date'
-              InputLabelProps={{ shrink: true }}
-            /> */}
           </Grid>
           <Grid item xs={12}>
             <InputController
               name='about_you'
-              label='About You'
+              label={dataForm.about_you}
               multiline
               rows={4}
             />
-            {/* <TextField
-              required
-              fullWidth
-              multiline
-              rows={4}
-              id='about_you'
-              label='About You'
-              aria-label='About You'
-              type='text'
-            /> */}
           </Grid>
           <Grid item xs={6}>
             <Button fullWidth variant='outlined' onClick={handleFormCancel}>
-              Cancel
+              {actionButtons.cancel}
             </Button>
           </Grid>
           <Grid item xs={6}>
             <Button fullWidth variant='contained' type='submit'>
-              Submit
+              {actionButtons.submit}
             </Button>
           </Grid>
         </Grid>
