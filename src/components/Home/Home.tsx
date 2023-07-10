7c5d02fdf2db1153c208836feee96a99911a15be
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-07-10 10:06:06 +0200

    Update Home.tsx

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,60 +1,28 @@
-import { useState } from 'react';
-import { Paper, Box, Tabs, Tab } from '@mui/material';
-import DataForm from './DataForm/DataForm';
+import { Paper, Box } from '@mui/material';
+import PersonalDataForm from './PersonalDataForm/PersonalDataForm';
+import TabPanel from '../Utils/TabsBox/TabPanel/TabPanel';
+import TabsBox from '../Utils/TabsBox/TabsBox';
+import useTabs from '../Utils/TabsBox/hooks/useTabs';
 
-interface TabPanelProps {
-  children?: React.ReactNode;
-  index: number;
-  value: number;
-}
-
-function TabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role='tabpanel'
-      hidden={value !== index}
-      id={`tabpanel-${index}`}
-      aria-labelledby={`Tab-${index}`}
-      {...other}>
-      {value === index && <Box>{children}</Box>}
-    </div>
-  );
-}
-
-function a11yProps(index: number) {
-  return {
-    id: `full-width-tabpanel-${index}`,
-    'aria-controls': `full-width-tabpanel-${index}`,
-  };
-}
+const tabLabels = [{ label: 'Data Form' }, { label: 'Data Table' }];
 
 const Home = () => {
-  const [value, setValue] = useState(0);
-
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-    setValue(newValue);
-  };
+  const { tabId, handleChangeTab } = useTabs();
 
   return (
     <Paper elevation={3}>
       <Box sx={{ width: '100%' }}>
-        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-          <Tabs
-            value={value}
-            onChange={handleChange}
-            variant='fullWidth'
-            aria-label='Data Tabs'>
-            <Tab label='Data Form' {...a11yProps(0)} />
-            <Tab label='Data Table' {...a11yProps(1)} />
-          </Tabs>
-        </Box>
+        <TabsBox
+          value={tabId}
+          handleChange={handleChangeTab}
+          tabLabels={tabLabels}
+          ariaLabel='Data Tabs'
+        />
         <Box padding={3}>
-          <TabPanel value={value} index={0}>
-            <DataForm />
+          <TabPanel value={tabId} index={0}>
+            <PersonalDataForm />
           </TabPanel>
-          <TabPanel value={value} index={1}>
+          <TabPanel value={tabId} index={1}>
             Item Two
           </TabPanel>
         </Box>
