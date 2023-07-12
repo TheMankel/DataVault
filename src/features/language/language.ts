f53ef3c71b7bc900cba404d1c8822baffe35a6ab
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-07-12 15:11:35 +0200

    Update language.ts

--- src/features/language/language.ts
+++ src/features/language/language.ts
@@ -6,14 +6,21 @@ interface ILanguage {
     code: string;
     urls: { main: string; data: string };
     dataForm: {
+      label: string;
       firstname: string;
       surname: string;
       date_of_birth: string;
       about_you: string;
     };
+    dataTable: {
+      label: string;
+    };
     actionButtons: {
       cancel: string;
       submit: string;
+    };
+    errors: {
+      required: string;
     };
   };
 }
@@ -24,14 +31,21 @@ const languageValues: ILanguage = {
     code: 'PL',
     urls: { main: 'Start', data: 'Dane' },
     dataForm: {
+      label: 'Formularz na Dane',
       firstname: 'Imię',
       surname: 'Nazwisko',
       date_of_birth: 'Data Urodzenia',
       about_you: 'O Tobie',
     },
+    dataTable: {
+      label: 'Tabela z danymi',
+    },
     actionButtons: {
       cancel: 'Anuluj',
       submit: 'Wyślij',
+    },
+    errors: {
+      required: 'Proszę podać swoje imię',
     },
   },
   GB: {
@@ -39,14 +53,21 @@ const languageValues: ILanguage = {
     code: 'GB',
     urls: { main: 'Home', data: 'Data' },
     dataForm: {
+      label: 'Data Form',
       firstname: 'Firstname',
       surname: 'Surname',
       date_of_birth: 'Date of Birth',
       about_you: 'About You',
     },
+    dataTable: {
+      label: 'Data Table',
+    },
     actionButtons: {
       cancel: 'Cancel',
       submit: 'Submit',
+    },
+    errors: {
+      required: 'Please provide your firstname',
     },
   },
 };
