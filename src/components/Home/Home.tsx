03f2374c6077f3aa90f1532fc97b36ebe4d2a6ac
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-08-10 21:00:40 +0200

    Components refactor

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -42,7 +42,7 @@ const Home = () => {
           tabLabels={tabLabels}
           ariaLabel='Data Tabs'
         />
-        <Box padding={3} mb={3}>
+        <Box mb={3} sx={{ padding: { xs: 1, sm: 2, md: 3 } }}>
           <TabPanel value={tabId} index={0}>
             <PersonalDataForm />
           </TabPanel>
