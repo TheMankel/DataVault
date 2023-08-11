8ed5831203928ed76448ceb280d3b8e6daa744ab
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-08-11 21:14:40 +0200

    Added theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,20 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import DataPage from './pages/DataPage';
 import AppLayout from './layouts/AppLayout';
+import { ThemeProvider } from '@mui/material';
+import theme from './theme/theme';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<AppLayout />}>
-        <Route index element={<HomePage />} />
-        <Route path='data' element={<DataPage />} />
-        {/* <Route path="*" element={<NoMatch />} /> */}
-      </Route>
-    </Routes>
+    <ThemeProvider theme={theme}>
+      <Routes>
+        <Route path='/' element={<AppLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path='data' element={<DataPage />} />
+          {/* <Route path="*" element={<NoMatch />} /> */}
+        </Route>
+      </Routes>
+    </ThemeProvider>
   );
 }
 
