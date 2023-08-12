27afedbd45a1cbc5a16fb2a69b9d486a13ed5f14
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-08-12 11:15:09 +0200

    Created PersonalDataCard

---
+++ src/components/PersonalData/PersonalDataCard/PersonalDataCard.tsx
@@ -0,0 +1,32 @@
+import { Card, CardContent, Typography } from '@mui/material';
+import { VaultData } from 'Types/PersonalDataType';
+
+interface IPersonalDataCardProps {
+  personData: VaultData;
+}
+
+const PersonalDataCard = ({ personData }: IPersonalDataCardProps) => {
+  return (
+    <Card
+      sx={{
+        height: 1,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+      }}>
+      <CardContent>
+        <Typography variant='h5' component='div'>
+          {personData.firstname} {personData.surname}
+        </Typography>
+        <Typography color='text.secondary' gutterBottom>
+          {personData.date_of_birth}
+        </Typography>
+        <Typography align='justify' variant='body2'>
+          {personData.about_you}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default PersonalDataCard;
