3007a302ed2cf99069e1304a167caea562fb25ee
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-08-12 11:15:49 +0200

    Added grid to display cards

--- src/components/PersonalData/PersonalData.tsx
+++ src/components/PersonalData/PersonalData.tsx
@@ -1,5 +1,21 @@
-const Data = () => {
-  return <div>Data</div>;
+import { Box, Grid } from '@mui/material';
+import { useAppSelector } from 'Store/hooks';
+import PersonalDataCard from './PersonalDataCard/PersonalDataCard';
+
+const PersonalData = () => {
+  const PersonalData = useAppSelector((state) => state.vault);
+
+  return (
+    <Box>
+      <Grid container spacing={3}>
+        {PersonalData.map((personData) => (
+          <Grid key={personData.id} item xs={12} sm={6} lg={4}>
+            <PersonalDataCard personData={personData} />
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
 };
 
-export default Data;
+export default PersonalData;
